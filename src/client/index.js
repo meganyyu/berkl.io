37abc47437f1516dc498ad92024e69281c62619c
Joon Park <joonqkr@berkeley.edu>
2021-10-24 07:13:09 -0700

    indicates when a bear has been clicked, error checking for when a bear has not been clicked

--- src/client/index.js
+++ src/client/index.js
@@ -1,18 +1,26 @@
 // SELECTION
 const blck_button = document.getElementById('blck');
-window.name = 'brwn';
+const brwn_button = document.getElementById('brwn');
+const h_button = document.getElementById('h');
 function set_blck() {
   window.name = 'blck';
+  blck_button.innerHTML = "<img src='public/assets/blckbear-hen-front-star.png'>";
+  brwn_button.innerHTML = "<img src='public/assets/brwnbear-hen-front.png'>";
+  h_button.innerHTML = "<img src='public/assets/hbear-hen-front.png'>";
 }
 blck_button.onclick = set_blck;
-const brwn_button = document.getElementById('brwn');
 function set_brwn() {
   window.name = 'brwn';
+  brwn_button.innerHTML = "<img src='public/assets/brwnbear-hen-front-star.png'>";
+  blck_button.innerHTML = "<img src='public/assets/blckbear-hen-front.png'>";
+  h_button.innerHTML = "<img src='public/assets/hbear-hen-front.png'>";
 }
 brwn_button.onclick = set_brwn;
-const h_button = document.getElementById('h');
 function set_h() {
   window.name = 'h';
+  h_button.innerHTML = "<img src='public/assets/hbear-hen-front-star.png'>";
+  blck_button.innerHTML = "<img src='public/assets/blckbear-hen-front.png'>";
+  brwn_button.innerHTML = "<img src='public/assets/brwnbear-hen-front.png'>";
 }
 h_button.onclick = set_h;
 
@@ -20,6 +28,10 @@ h_button.onclick = set_h;
 const play_button = document.getElementById('play');
 // Clicking the play button leads to main game page
 function play() {
+  if (window.name != 'blck' || window.name != 'brwn' || window.name != 'h') {
+    alert("Please choose a type of bear!");
+    return;
+  }
   window.location.href = 'src/client/html/game.html';
 }
 play_button.onclick = play;
