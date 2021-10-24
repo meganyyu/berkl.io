ceeaea6f7c8511dd25cb3d6c0e76b51342ecec08
Joon Park <joonqkr@berkeley.edu>
2021-10-23 23:43:45 -0700

    added background animation

--- src/client/landing.js
+++ src/client/landing.js
@@ -1,4 +1,5 @@
-var play_button = document.getElementById('play');
+// Play button leads to main game page.
+const play_button = document.getElementById('play');
 
 function play() {
   window.location.href = 'index.html';
